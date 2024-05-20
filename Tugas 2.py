2f776d777b072d70d79b32c9e9c8dd6468044afa
yunav2 <yunaodasteam@gmail.com>
2024-05-20 10:28:03 +0700

    Tugas 2

---
+++ Tugas 2.py
@@ -0,0 +1,13 @@
+# Inputan
+n = int(input("Masukkan Nilai n : ")) 
+
+for i in range(n) : 
+            print (i ** 2)
+
+print("---------------------------------")
+
+for i in range(n) :
+         if i % 2 or i == 0 : 
+            print (i ** 2)
+            
+        
